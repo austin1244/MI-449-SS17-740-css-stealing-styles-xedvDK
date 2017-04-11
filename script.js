16c584153fc99bd23309907959891478dfd2d98c
Austin1244 <rixausti@msu.edu>
2017-04-11 19:34:28 -0400

    updates

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-function getRand(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
+(function ($) {
+  function getRand (min, max) {
+    return Math.floor(Math.random() * (max - min)) + min
+  }
 
-$(document).ready(function () {
-  var classes = [
-    "green","blue","yellow","purple","red"
-  ]
-  $(".search-name").find("span").each(function () {
-    $(this).addClass(classes[getRand(0, classes.length)])
+  $(document).ready(function () {
+    var classes = [
+      'green', 'blue', 'yellow', 'purple', 'red'
+    ]
+    $('.search-name').find('span').each(function () {
+      $(this).addClass(classes[getRand(0, classes.length)])
+    })
   })
-
-});
+})(jQuery)
