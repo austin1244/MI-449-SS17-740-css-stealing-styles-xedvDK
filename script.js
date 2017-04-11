7c2c2a934755fc1b095a875c622d4e509ca33460
Austin Rix <rixausti@black1.cse.msu.edu>
2017-04-11 15:19:50 -0400

    updates

---
+++ script.js
@@ -0,0 +1,13 @@
+function getRand(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+$(document).ready(function () {
+  var classes = [
+    "green","blue","yellow","purple","red"
+  ]
+  $(".search-name").find("span").each(function () {
+    $(this).addClass(classes[getRand(0, classes.length)])
+  })
+
+});
